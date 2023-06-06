03528458def0bac579c4bdf6722d2481effacfd3
amg1688 <anamariagalvez1688@gmail.com>
2023-06-06 16:49:17 -0300

    subida de archivo es_palindromo.py

---
+++ es_palindromo.py
@@ -0,0 +1,22 @@
+print("""BIENVENIDO
+Esta pequeña aplicación te dirá si la palabra ingresada es o no
+un palíndromo (palabra que se puede leer de izquierda a derecha y
+de derecha a izquierda)""")
+
+
+continuar=True
+palabra=input("Ingresa la palabra. Para salir escribe 'salir': ")
+while continuar:
+    
+
+    if palabra!=palabra[::-1] and palabra!="salir":
+        print(f"La palabra {palabra} NO ES PALÍNDROMO")
+        palabra=input("Ingresa la palabra. Para salir escribe 'salir': ")
+    elif palabra==palabra[::] and palabra!="salir":
+        print(f"La palabra {palabra} ES PALÍNDROMO")
+        palabra=input("Ingresa la palabra. Para salir escribe 'salir': ")
+    else:
+        continuar=False
+    
+            
+print("GRACIAS POR JUGAR")

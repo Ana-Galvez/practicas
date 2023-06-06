a7ba2eac7ac6100e1eb319a687a661cc28150169
amg1688 <anamariagalvez1688@gmail.com>
2023-06-06 15:32:21 -0300

    subida de archivo cuantas_vocales_tiene.py

---
+++ cuantas_vocales_tiene.py
@@ -0,0 +1,14 @@
+print("""BIENVENIDO
+Esta pequeña aplicación te dirá la cantidad de vocales que
+tiene tu oración""")
+
+vocales="aeiouáéíóú"
+oracion=input("Ingresa la oración: ")
+oracion_minuscula=oracion.lower()
+contador_de_vocales=0
+
+for letra in oracion_minuscula:
+    if letra in vocales:
+        contador_de_vocales+=1
+
+print(f"La cantidad de vocales que tiene tu oración: {contador_de_vocales}")

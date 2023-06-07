e7d7324cc65114a6c196b35e352501935173e5af
amg1688 <anamariagalvez1688@gmail.com>
2023-06-07 19:18:05 -0300

    haciendolo con funciones

--- adivinar_numero_aleatorio.py
+++ adivinar_numero_aleatorio.py
@@ -1,19 +1,24 @@
 import random as rd
 
-print("""BIENVENIDO
+def num_aleatorio():
+    numero_aleatorio=rd.randint(1,100)
+    numero=int(input("Ingresa un número: "))
+    contador_de_intentos=0
+
+    while numero_aleatorio!=numero:
+        if numero_aleatorio<numero:
+            print("El número aleatorio es MENOR al que pusiste")
+            numero=int(input("Ingresa un número: "))
+        elif numero_aleatorio>numero:
+            print("El número aleatorio es MAYOR al que pusiste")
+            numero=int(input("Ingresa un número: "))
+        contador_de_intentos+=1
+    print(f"FELICIDADES!!! adivinaste el número al {contador_de_intentos} intento")
+
+def main():
+    print("""BIENVENIDO
 Este pequeño programa crea un número aleatorio y te dará pistas
 ¿Podrás adivinarlo? El número está entre 1 y 100""")
+    num_aleatorio()
 
-numero_aleatorio=rd.randint(1,100)
-ingresa_numero=int(input("Ingresa un número: "))
-contador_de_intentos=0
-
-while numero_aleatorio!=ingresa_numero:
-    if numero_aleatorio<ingresa_numero:
-        print("El número aleatorio es MENOR al que pusiste")
-        ingresa_numero=int(input("Ingresa un número: "))
-    elif numero_aleatorio>ingresa_numero:
-        print("El número aleatorio es MAYOR al que pusiste")
-        ingresa_numero=int(input("Ingresa un número: "))
-    contador_de_intentos+=1
-print(f"FELICIDADES!!! adivinaste el número al {contador_de_intentos} intento")
+main()

48eaec7fbf79f067be11d6f7bd81fe491384e701
amg1688 <anamariagalvez1688@gmail.com>
2023-06-07 19:25:03 -0300

    haciendolo con funciones

--- cuantas_vocales_tiene.py
+++ cuantas_vocales_tiene.py
@@ -1,14 +1,19 @@
-print("""BIENVENIDO
+def cuantas_vocales():
+    vocales="aeiouáéíóú"
+    oracion=input("Ingresa la oración: ")
+    oracion_minuscula=oracion.lower()
+    contador_de_vocales=0
+
+    for letra in oracion_minuscula:
+        if letra in vocales:
+            contador_de_vocales+=1
+
+    print(f"La cantidad de vocales que tiene tu oración: {contador_de_vocales}")
+
+def main():
+    print("""BIENVENIDO
 Esta pequeña aplicación te dirá la cantidad de vocales que
 tiene tu oración""")
+    cuantas_vocales()
 
-vocales="aeiouáéíóú"
-oracion=input("Ingresa la oración: ")
-oracion_minuscula=oracion.lower()
-contador_de_vocales=0
-
-for letra in oracion_minuscula:
-    if letra in vocales:
-        contador_de_vocales+=1
-
-print(f"La cantidad de vocales que tiene tu oración: {contador_de_vocales}")
+main()

a697c3314b2f94ca866d57daa9dd8d81e250f29d
amg1688 <anamariagalvez1688@gmail.com>
2023-06-07 18:56:41 -0300

    subiendo numero_primo_o_no.py

---
+++ numero_primo_o_no.py
@@ -0,0 +1,14 @@
+def primo_o_no(numero):
+    for n in range(2, numero):
+        if numero % n == 0:
+            return f"El numero {numero} NO ES PRIMO"
+    return f"El número {numero} es primo"
+    
+    
+def main():
+    print("""BIENVENID@S
+    Esta pequeña aplicación te dirá si el número elegido es o no un número primo""")
+    numero=int(input("Ingrese el número: "))
+    print(primo_o_no(numero))
+
+main()

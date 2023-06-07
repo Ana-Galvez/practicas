582ba09e8f5d71aa8a9e47d3463d127cac388931
amg1688 <anamariagalvez1688@gmail.com>
2023-06-07 20:22:27 -0300

    archivo saber_divisores.py

---
+++ saber_divisores.py
@@ -0,0 +1,15 @@
+def divisores(numero):
+    lista=[]
+    for i in range(1, numero+1):
+        if numero % i == 0:
+            lista.append(i)
+    return lista
+
+def main():
+    print("""BIENVENID@
+    Este pequeño programa te mostrará todos los divisores que posee el número elegido""")
+    
+    numero=int(input("ingresa numero: "))
+    print(f"Los divisores del numero {numero} son:\n{divisores(numero)}")
+
+main()

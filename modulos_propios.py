75a06674ef11c59024a1d0e610aa58a83dd00f94
amg1688 <anamariagalvez1688@gmail.com>
2023-06-09 17:17:10 -0300

    subida de archivos modulos_propios.py con funcione de potencia y raiz y otro archivo main usando los modulos

---
+++ modulos_propios.py
@@ -0,0 +1,16 @@
+def potencia ()->float:
+    """Función que el usuario agrega la base y la potencia y devuelve el resultado"""
+    from math import pow
+    agregar_base=int(input("Ingresa el número que será la base: "))
+    agregar_potencia=int(input("Ingresa el número que será la potencia: "))
+    resultado=pow(agregar_base,agregar_potencia)
+    return resultado 
+
+def raiz ()->float:
+    """Función para sacar la raiz de cualquier número pedido por el usuario"""
+    from math import pow
+    agregar_radicando=int(input("Ingresa el número como radicando: "))
+    agregar_indice=int(input("Ingresa el número como índice: "))
+    resultado=pow(agregar_radicando,(1/agregar_indice))
+    return resultado
+

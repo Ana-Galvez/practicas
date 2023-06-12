2c415c2abe663aa7645448acf215f0707198a044
amg1688 <anamariagalvez1688@gmail.com>
2023-06-12 16:45:23 -0300

    arreglo en los archivos

--- modulos_propios_con_fechas.py
+++ modulos_propios_con_fechas.py
@@ -1,9 +1,7 @@
-def cuantos_dias_faltan()->int:
-    """Función donde el usuario pone mes y día y devuelve los días que faltan"""
+def cuantos_dias_faltan(mes:int,dia:int)->int:
+    """Función donde se pide mes y día y devuelve los días que faltan"""
     import datetime
-    mes=int(input("Ingresa el mes de tu nacimiento: "))
-    dia=int(input("Ingresa el día de tu nacimiento: "))
-
+    
     hoy=datetime.date.today()
     fecha=datetime.date(hoy.year,mes,dia)
 
@@ -13,15 +11,26 @@ def cuantos_dias_faltan()->int:
     tiempo_que_falta_en_dias=tiempo_que_falta.days
     return tiempo_que_falta_en_dias
 
-def que_dia_fue_o_sera()->str:
-    """Función donde el usuario pone una fecha y devuelve que día fue o será"""
+def que_dia_fue_o_sera(fecha:str)->str:
+    """Función donde se pide una fecha y devuelve que día fue o será"""
     import datetime
     dias=["Lunes","Martes","Miércoles","Jueves","Viernes","Sábado","Domingo"]
-    anio=int(input("Ingresa el año: "))
-    mes=int(input("Ingresa el mes: "))
-    dia=int(input("Ingresa el día: "))
+    dia,mes,anio=map(int,fecha.split('/'))
 
     fecha=datetime.date(anio,mes,dia)
     que_dia=fecha.weekday()
     que_dia_es= dias[que_dia]
     return que_dia_es
+
+def calculadora_de_dias(fecha_comienzo:str,fecha_final:str)->int:
+    """Función donde se pide 2 fechas y saca la diferencia en días"""
+    import datetime
+    diac,mesc,anioc= map(int,fecha_comienzo.split('/'))
+    diaf,mesf,aniof= map(int,fecha_final.split('/'))
+
+    fecha_comienzo_date=datetime.date(anioc,mesc,diac)
+    fecha_final_date=datetime.date(aniof,mesf,diaf)
+
+    resultado=fecha_final_date-fecha_comienzo_date
+    dias=resultado.days
+    return dias
--- proyectos_con_fechas.py
+++ proyectos_con_fechas.py
@@ -1,4 +1,3 @@
-"""1- Calculadora de días entre fechas: Crea un programa que solicite al usuario dos fechas y calcule la cantidad de días transcurridos entre ellas"""
 """2- Días vividos: Pide al usuario su fecha de nacimiento y muestra cuántos días ha vivido hasta la fecha actual"""
 """3- Calculadora de edad: Crea un programa que solicite al usuario su fecha de nacimiento y calcule su edad en años, meses y días"""
 """4- Cronómetro: Implementa un cronómetro en Python que calcule el tiempo transcurrido entre un punto de inicio y un punto de parada, mostrando los resultados en horas, minutos y segundos"""
@@ -6,7 +5,36 @@
 """6- Conversor de formatos de fecha: Escribe un programa que solicite al usuario una fecha en un formato específico (por ejemplo, "dd/mm/aaaa") y lo convierta a otros formatos comunes, como "mm/dd/aaaa" o aaaa-mm-dd"""
 
 
+import modulos_propios_con_fechas
 
+def main():
+    """Proyectos con fechas"""
+    print("""BIENVENID@: Este programa te servirá si quieres trabajar con fechas \n""")
+    
+    ingreso_while=True
+    while ingreso_while:
+        print("""¿Que quieres hacer?
+    1- Saber cuántos días faltan
+    2- Qué día fue o será
+    3- Calcular cuantos días entre 2 fechas
+    4- Salir
+    """)
+        opcion=int(input("Elije la opción con el número correspondiente: "))
+        if opcion==1:
+            mes=int(input("Ingresa el mes de tu nacimiento: "))
+            dia=int(input("Ingresa el día de tu nacimiento: "))
+            dias_que_faltan=modulos_propios_con_fechas.cuantos_dias_faltan(mes,dia)
+            print(f"Faltan {dias_que_faltan} días\n -------------")
+        elif opcion==2:
+            fecha=input("Ingresa la fecha como dia/mes/año: ")
+            que_dia=modulos_propios_con_fechas.que_dia_fue_o_sera(fecha)
+            print(f"Ese día cayó {que_dia}\n ----------------")
+        elif opcion==3:
+            fecha_inicio=input("Ingresa la fecha más lejana como dia/mes/año: ")
+            fecha_final=input("Ingresa la fecha más cercana como dia/mes/año: ")
+            saber_dias=modulos_propios_con_fechas.calculadora_de_dias(fecha_inicio,fecha_final)
+            print(f"Pasaron {saber_dias} días\n ----------------")
+        elif opcion==4:
+            ingreso_while=False
 
-def calculadora_dias_entre_fechas(fecha1,fecha2):
-    """Función que pide 2 fechas al usuario y retorna la diferencia en días"""
+main()
